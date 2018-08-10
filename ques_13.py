0bfa085aeab0c59f9bab47a7ed0c8d2ca1e5b482
nitinkothari17 <30491855+nitinkothari17@users.noreply.github.com>
2018-08-10 12:08:12 +0530

    Update ques_13.py

--- ques_13.py
+++ ques_13.py
@@ -1,3 +1,8 @@
+"""Q13
+Here's another one (12th was too easy)
+1. Write a recursive function to return 100th fibonacci number.
+2. Optimize the function to reduce execution time. """
+
 first = 0 
 second = 1
 for i in range(2,100):
