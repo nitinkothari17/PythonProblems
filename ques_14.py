0745177997d759d90e1560597ad987bc35d08afc
nitinkothari17 <30491855+nitinkothari17@users.noreply.github.com>
2018-08-27 10:47:52 +0530

    Update ques_14.py

--- ques_14.py
+++ ques_14.py
@@ -1,7 +1,6 @@
 from collections import namedtuple
 from operator import attrgetter
 
-tuples = namedtuple('tuples' , 'name age height')
-seq = [tuples("Tom",19,80),tuples("John",20,90),tuples("Jony",17,91),tuples("Jony",17,93),tuples("Json",21,85)]
+seq = [("Tom",19,80),("John",20,90),("Jony",17,91),("Jony",17,93),("Json",21,85)]
 print sorted(seq)
  
