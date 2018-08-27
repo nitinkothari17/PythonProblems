ed3b91c8d3785b858b3b21ac51a6293e31b0cb04
nitinkothari17 <30491855+nitinkothari17@users.noreply.github.com>
2018-08-27 10:38:41 +0530

    Update ques_14.py

--- ques_14.py
+++ ques_14.py
@@ -1,17 +1,7 @@
-def swap(tuples,i):
-	a = tuples[i]
-	tuples[i] = tuples[i+1]
-	tuples[i+1] = a
-	
-tuples = [("Tom",19,80),("John",20,90),("Jony",17,91),("Jony",17,93),("Json",21,85)]
-for i in xrange(len(tuples)-1):
-	if tuples[i][0] > tuples[i+1][0]:
-		swap(tuples,i)
-		continue
-	if tuples[i][1] > tuples[i+1][1]: 
-		swap(tuples,i)
-		continue
-	if tuples[i][2] > tuples[i+1][2]: 
-		swap(tuples,i)
-		continue
-print tuples
+from collections import namedtuple
+from operator import attrgetter
+
+tuples = namedtuple('tuples' , 'name age height')
+seq = [tuples("Tom",19,80),tuples("John",20,90),tuples("Jony",17,91),tuples("Jony",17,93),tuples("Json",21,85)]
+print sorted(seq)
+ 
